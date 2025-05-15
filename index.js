cfb7eb5bb612f92443a4f45b7f93f463c6f7b864
Ömer Faruk BORAN <46742751+farukborann@users.noreply.github.com>
2025-05-15 19:27:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -473,15 +473,13 @@ function Windows(instance, callback) {
       instance.pathStdout = Node.path.join(instance.path, 'stdout');
       instance.pathStderr = Node.path.join(instance.path, 'stderr');
       instance.pathStatus = Node.path.join(instance.path, 'status');
-      console.log("## [SUDO] Instance", { instance })
+
       Node.fs.mkdir(instance.path,
         { recursive: true },
         function(error) {
-          console.log("## [After MKDIR]", { error })
           if (error) return callback(error);
-          console.log("## [After callback]")
+
           function end(error, stdout, stderr) {
-            console.log("## [END]", { end, stdout, stderr, instance })
             Remove(instance.path,
               function(errorRemove) {
                 if (error) return callback(error, stdout, stderr);
@@ -490,27 +488,23 @@ function Windows(instance, callback) {
               }
             );
           }
-          console.log("## [1]")
+
           WindowsWriteExecuteScript(instance,
             function(error) {
-              console.log("## [2]", { error })
               if (error) return end(error);
-              console.log("## [3]")
+
               WindowsWriteCommandScript(instance,
                 function(error) {
-                  console.log("## [4]", { error })
                   if (error) return end(error);
-                  console.log("## [5]")
+
                   WindowsElevate(instance,
                     function(error, stdout, stderr) {
-                      console.log("## [6]", { error, stdout, stderr })
                       if (error) return end(error, stdout, stderr);
-                      console.log("## [7]")
+
                       WindowsWaitForStatus(instance,
                         function(error) {
-                          console.log("## [8]", { error })
                           if (error) return end(error);
-                          console.log("## [9]")
+
                           WindowsResult(instance, end);
                         }
                       );
@@ -541,7 +535,7 @@ function WindowsElevate(instance, end) {
   command.push('-WindowStyle hidden');
   command.push('-Verb runAs');
   command = command.join(' ');
-  console.log("Elevate Command", command)
+
   var child = Node.child.exec(command, { encoding: 'utf-8' },
     function(error, stdout, stderr) {
       // We used to return PERMISSION_DENIED only for error messages containing
@@ -549,7 +543,6 @@ function WindowsElevate(instance, end) {
       // error messages (issue 96) so now we must assume all errors here are
       // permission errors. This seems reasonable, given that we already run the
       // user's command in a subshell.
-      console.log("# WindowsElevate Error", { error, stdout, stderr })
       if (error) return end(new Error(PERMISSION_DENIED), stdout, stderr);
       end();
     }
@@ -560,15 +553,12 @@ function WindowsElevate(instance, end) {
 function WindowsResult(instance, end) {
   Node.fs.readFile(instance.pathStatus, 'utf-8',
     function(error, code) {
-      console.log("Read instance.pathStatus", { error, code })
       if (error) return end(error);
       Node.fs.readFile(instance.pathStdout, 'utf-8',
         function(error, stdout) {
-          console.log("WindowsResultError", { error, stdout })
           if (error) return end(error);
           Node.fs.readFile(instance.pathStderr, 'utf-8',
             function(error, stderr) {
-              console.log("Read pathStderr", { instance, error, stderr })
               if (error) return end(error);
               code = parseInt(code.trim(), 10);
               if (code === 0) {
@@ -594,10 +584,8 @@ function WindowsWaitForStatus(instance, end) {
   // PowerShell can be used to elevate and wait on Windows 10.
   // PowerShell can be used to elevate on Windows 7 but it cannot wait.
   // powershell.exe Start-Process cmd.exe -Verb runAs -Wait
-  console.log("TRY WindowsWaitForStatus", instance.pathStatus)
   Node.fs.stat(instance.pathStatus,
     function(error, stats) {
-      console.log("1 WindowsWaitForStatus", {error, stats})
       if ((error && error.code === 'ENOENT') || stats.size < 2) {
         // Retry if file does not exist or is not finished writing.
         // We expect a file size of 2. That should cover at least "0\r".
@@ -608,10 +596,8 @@ function WindowsWaitForStatus(instance, end) {
             // If administrator has no password and user clicks Yes, then
             // PowerShell returns no error and execute (and command) never runs.
             // We check that command output has been redirected to stdout file:
-            console.log("TRY WindowsWaitForStatus", instance.pathStdout)
             Node.fs.stat(instance.pathStdout,
               function(error) {
-                console.log("2 Error", error)
                 if (error) return end(new Error(PERMISSION_DENIED));
                 WindowsWaitForStatus(instance, end);
               }
@@ -656,20 +642,8 @@ function WindowsWriteCommandScript(instance, end) {
   }
   script.push(instance.command);
   script = script.join('\r\n');
-  console.log("WindowsWriteCommandScript", { pathCommand: instance.pathCommand, script })
-  Node.fs.writeFile(instance.pathCommand, script, 'utf-8', end);
-}
 
-function WindowsWriteElevateScript(instance, end) {
-  // We do not use VBScript to elevate since it does not return an error if
-  // the user does not grant permission. This is here for reference.
-  // var script = [];
-  // script.push('Set objShell = CreateObject("Shell.Application")');
-  // script.push(
-  // 'objShell.ShellExecute "' + instance.pathExecute + '", "", "", "runas", 0'
-  // );
-  // script = script.join('\r\n');
-  // Node.fs.writeFile(instance.pathElevate, script, 'utf-8', end);
+  Node.fs.writeFile(instance.pathCommand, script, 'utf-8', end);
 }
 
 function WindowsWriteExecuteScript(instance, end) {
